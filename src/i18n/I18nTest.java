c31541f98d1d70b4107ce6e2512a2cea55f698e4
QtKaii <21qtkai@gmail.com>
2025-09-01 11:47:04 +0100

    added a test to make sure I could get the resources loaded in

---
+++ src/i18n/I18nTest.java
@@ -0,0 +1,36 @@
+package i18n;
+
+import logging.AssignmentLogger;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18nTest {
+    public static void main(String[] args) {
+        AssignmentLogger.logMain();
+
+        Locale en = Locale.ENGLISH;
+        Locale es = Locale.forLanguageTag("es");
+
+        ResourceBundle enBundle = ResourceBundle.getBundle("internationalization.MessagesBundle", en);
+        ResourceBundle esBundle = ResourceBundle.getBundle("internationalization.MessagesBundle", es);
+
+        printBundle("EN", enBundle);
+        printBundle("ES", esBundle);
+    }
+
+    private static void printBundle(String label, ResourceBundle bundle) {
+        AssignmentLogger.logStaticMethodEntry();
+        System.out.println("[" + label + "]");
+        System.out.println("title=" + bundle.getString("gui.title"));
+        System.out.println("inputLabel=" + bundle.getString("gui.inputLabel"));
+        System.out.println("searchButton=" + bundle.getString("gui.searchButton"));
+        System.out.println("playButton=" + bundle.getString("gui.playButton"));
+        System.out.println("langButton=" + bundle.getString("gui.langButton"));
+        System.out.println("guitar=" + bundle.getString("instrument.guitar"));
+        System.out.println("piano=" + bundle.getString("instrument.piano"));
+        System.out.println("drum=" + bundle.getString("instrument.drum"));
+        System.out.println("error.invalid=" + bundle.getString("error.invalid"));
+        AssignmentLogger.logStaticMethodExit();
+    }
+}
